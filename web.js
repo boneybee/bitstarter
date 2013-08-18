82a61afab2a82fd4d11e3dad397b1b6904df56dd
Ubuntu <ubuntu@ip-172-31-41-94.us-west-2.compute.internal>
2013-08-18 19:29:17 +0000

    adds Nigel

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World and Nigel Campbell!!!!');
 });
 
 var port = process.env.PORT || 5000;
