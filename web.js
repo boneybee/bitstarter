fa2cce2869a7987841f0b466f1d8905a1ebf23e7
Ubuntu <ubuntu@ip-172-31-41-94.us-west-2.compute.internal>
2013-08-18 19:59:47 +0000

    new line in web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World and Nigel Campbell!!!!');
+  response.send('Hello World and Nigel Campbell!!!!/n this is your day');
 });
 
 var port = process.env.PORT || 5000;
