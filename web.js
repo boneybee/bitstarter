174b547f546522586cc619f1e0682a7bbcbc98bb
Ubuntu <ubuntu@ip-172-31-32-169.us-west-2.compute.internal>
2013-08-23 20:02:23 +0000

    Friday number 2

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World and Nigel C this is your day and what a fine day it is. Will you be going for a walk? Again???  Oh what a fantastic moment, It worked.');
+  response.send('Hello World and Nigel C this is your day and what a fine day it is. Will you be going for a walk? Again???  Oh what a fantastic moment, It worked. Well now try again.');
 });
 
 var port = process.env.PORT || 5000;
